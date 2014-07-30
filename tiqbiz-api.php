b1dbd95f224210e3742a9eb4f97527d82c6e5bf1
tiqbiz <tiqbiz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-30 06:44:20 +0000

    CalPress Pro support
    git-svn-id: https://plugins.svn.wordpress.org/tiqbiz-api/trunk@957169 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tiqbiz-api.php
+++ tiqbiz-api.php
@@ -20,6 +20,8 @@ define('TIQBIZ_API_PLUGIN_PATH', __FILE__);
 define('TIQBIZ_API_PLUGIN_BASE', plugin_basename(TIQBIZ_API_PLUGIN_PATH));
 
 define('TIQBIZ_API_EVENT_PLUGIN', 'calpress-event-calendar/calpress.php');
+define('TIQBIZ_API_EVENT_PLUGIN_PRO', 'calpress-pro/calpress-pro.php');
+
 define('TIQBIZ_API_EVENT_CLASS', '\Calp_Event');
 
 spl_autoload_register(function($class) {
